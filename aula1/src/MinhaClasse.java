ba292fd3114c87469545e40fa2f5d4256935e269
Juliana Girotto <ads.jjgirotto@gmail.com>
2023-08-20 17:48:02 -0300

    minha classe - meu nome

---
+++ aula1/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+
+    // método principal
+    public static void main(String[] args) { 
+        System.out.print("Olá, mundo!\n"); // imprimindo
+        String primeiroNome = "Juliana"; // declarando variável
+        String segundoNome = "Girotto"; // declarando variável
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome); // declarando variável com valor de método
+
+        System.out.println(nomeCompleto); // imprimindo a variável
+    }
+
+    // declarando o método do nome completo
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
