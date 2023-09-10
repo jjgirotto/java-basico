aa50b88602551273aab96bd73cdd3808ec00fb6f
Juliana Girotto <ads.jjgirotto@gmail.com>
2023-09-10 18:45:00 -0300

    collections set com hashset

---
+++ collections/Set/Operações Básicas/Conjunto de Convidados/src/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,58 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavrasUnicas {
+    private Set<String> palavraSet;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavraSet = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra) {
+        palavraSet.add(palavra);
+    }
+
+    public void removerPalavra(String palavra) {
+        if (!palavraSet.isEmpty()) {
+            if (palavraSet.contains(palavra)) {
+                palavraSet.remove(palavra);
+            }
+            else
+                System.out.println("Palavra não encontrada no conjunto!");
+        }
+        else
+            System.out.println("O conjunto está vazio!");
+    }
+
+    public boolean verificarPalavra(String palavra) {
+        return palavraSet.contains(palavra);
+    }
+
+    public void exibirPalavrasUnicas() {
+        if (!palavraSet.isEmpty()) {
+            System.out.println(palavraSet);
+        }
+        else
+            System.out.println("O conjunto está vazio!");
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjuntoPalavras = new ConjuntoPalavrasUnicas();
+
+        conjuntoPalavras.exibirPalavrasUnicas();
+
+        conjuntoPalavras.adicionarPalavra("Livro");
+        conjuntoPalavras.adicionarPalavra("Mesa");
+        conjuntoPalavras.adicionarPalavra("Cadeira");
+        conjuntoPalavras.adicionarPalavra("Cama");
+
+        conjuntoPalavras.exibirPalavrasUnicas();
+
+        conjuntoPalavras.removerPalavra("Cama");
+        conjuntoPalavras.exibirPalavrasUnicas();
+
+        System.out.println(conjuntoPalavras.verificarPalavra("Livro"));
+    }
+}
+
+
